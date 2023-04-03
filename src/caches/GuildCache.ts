8274bfbe7f684b70186d48ac0e582d5b47f0cc8d
Toby Chambers <74092151+thepiex@users.noreply.github.com>
2023-04-03 13:10:06 +0100

    Initial Commit
    Initial Commit

---
+++ src/caches/GuildCache.ts
@@ -0,0 +1,11 @@
+export class GuildCache {
+    constructor() { }
+
+    public async get(id: string) { }
+
+    public async create() { }
+
+    public async delete() { }
+
+    public async update() { }
+}
